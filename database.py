33771d581865514c14cdede1ff55968751d7421e
root <root@a002el.fvds.ru>
2023-03-03 12:41:18 +0300

    Add typing module

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import glob
 import os
 import sqlite3
-
+from typing import List, Dict
 
 class Database:
     def __init__(self, database_filename: str) -> None:
@@ -13,7 +13,7 @@ class Database:
         self.conn.close()
 
     @staticmethod
-    def get_databases() -> list[str]:
+    def get_databases() -> List[str]:
         return [os.path.basename(path) for path in glob.glob("databases/*.db")]
 
     @staticmethod
@@ -28,25 +28,25 @@ class Database:
         if os.path.exists(file_path):
             os.remove(file_path)
 
-    def get_tables(self) -> list[str]:
+    def get_tables(self) -> List[str]:
         self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
         tables = list(map(lambda x: x[0], self.cursor.fetchall()))
 
         return tables
 
-    def get_info_columns_of_table(self, table_name: str) -> list[tuple]:
+    def get_info_columns_of_table(self, table_name: str) -> List[tuple]:
         self.cursor.execute(f"PRAGMA table_info({table_name})")
         result = self.cursor.fetchall()
 
         return result
 
-    def get_rows_from_table(self, table_name: str) -> list[tuple]:
+    def get_rows_from_table(self, table_name: str) -> List[tuple]:
         self.cursor.execute(f"SELECT * FROM {table_name}")
         result = self.cursor.fetchall()
 
         return result
 
-    def create_table(self, table_name: str, columns: list[str]) -> None:
+    def create_table(self, table_name: str, columns: List[str]) -> None:
         sql_columns = ", ".join([f"{i} TEXT" for i in columns])
         sql = f"""
 CREATE TABLE IF NOT EXISTS {table_name} (
@@ -62,7 +62,7 @@ CREATE TABLE IF NOT EXISTS {table_name} (
         self.cursor.execute(f"DROP TABLE IF EXISTS {table_name}")
         self.conn.commit()
 
-    def add_row_to_table(self, table_name, data: dict[str, str]) -> bool:
+    def add_row_to_table(self, table_name, data: Dict[str, str]) -> bool:
         columns = list(data.keys())
         values = list(data.values())
 
@@ -81,7 +81,7 @@ CREATE TABLE IF NOT EXISTS {table_name} (
         self.cursor.execute(f"DELETE FROM {table_name} WHERE id = ?", (id, ))
         self.conn.commit()
 
-    def get_table_rows(self, table: str) -> list[dict]:
+    def get_table_rows(self, table: str) -> List[dict]:
         self.conn.row_factory = Database.dict_factory
         cursor = self.conn.cursor()
         cursor.execute(f'SELECT * FROM {table}')
